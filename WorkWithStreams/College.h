a66321ed98ac42fadc907eec22ddddd795566cf0
login <nikolovi155@gmail.com>
2025-01-26 18:12:19 +0200

    Task4

---
+++ WorkWithStreams/College.h
@@ -0,0 +1,86 @@
+﻿#pragma once
+#include <string>
+#include <fstream>
+#include "ISerializable.h"
+
+class College : public ISerializable {
+private:
+    std::string name;
+    std::string address;
+    int foundationYear;
+    int studentsCount;
+
+public:
+    College() : name(""), address(""), foundationYear(0), studentsCount(0) {}
+
+    College(const std::string& n, const std::string& addr, int year, int students)
+        : name(n), address(addr), foundationYear(year), studentsCount(students) {}
+
+    void output() const {
+        std::cout << "College Name: " << name << std::endl;
+        std::cout << "Address: " << address << std::endl;
+        std::cout << "Foundation Year: " << foundationYear << std::endl;
+        std::cout << "Students Count: " << studentsCount << std::endl;
+    }
+
+    std::ostream& Serialize(std::ostream& output) override {
+        size_t nameSize = name.length();
+        size_t addressSize = address.length();
+
+        output.write(reinterpret_cast<char*>(&nameSize), sizeof(nameSize));
+        output.write(name.c_str(), nameSize);
+
+        output.write(reinterpret_cast<char*>(&addressSize), sizeof(addressSize));
+        output.write(address.c_str(), addressSize);
+
+        output.write(reinterpret_cast<const char*>(&foundationYear), sizeof(foundationYear));
+        output.write(reinterpret_cast<const char*>(&studentsCount), sizeof(studentsCount));
+
+        return output;
+    }
+
+    std::istream& Deserialize(std::istream& input) override {
+        size_t nameSize, addressSize;
+
+        input.read(reinterpret_cast<char*>(&nameSize), sizeof(nameSize));
+        name.resize(nameSize);
+        input.read(&name[0], nameSize);
+
+        input.read(reinterpret_cast<char*>(&addressSize), sizeof(addressSize));
+        address.resize(addressSize);
+        input.read(&address[0], addressSize);
+
+        input.read(reinterpret_cast<char*>(&foundationYear), sizeof(foundationYear));
+        input.read(reinterpret_cast<char*>(&studentsCount), sizeof(studentsCount));
+
+        return input;
+    }
+
+    std::ostream& Serialize(std::string& path) override {
+        std::ofstream file(path, std::ios::binary);
+        if (!file) {
+            std::cerr << "Error opening file for serialization" << std::endl;
+            return file;
+        }
+        return Serialize(file);
+    }
+
+    std::istream& Deserialize(std::string& path) override {
+        std::ifstream file(path, std::ios::binary);
+        if (!file) {
+            std::cerr << "Error opening file for deserialization" << std::endl;
+            return file;
+        }
+        return Deserialize(file);
+    }
+
+    std::ostream& Serialize() override {
+        std::string defaultPath = "college_serialization.bin";
+        return Serialize(defaultPath);
+    }
+
+    std::istream& Deserialize() override {
+        std::string defaultPath = "college_serialization.bin";
+        return Deserialize(defaultPath);
+    }
+};
--- WorkWithStreams/Main.cpp
+++ WorkWithStreams/Main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <iostream>
 #include "String.h"
 #include "Contacts.h"
+#include "College.h"
 
 int main() {
     
@@ -17,7 +18,7 @@ int main() {
     std::cout << "Deserialized string: ";
     s2.output();*/
 
-    Contacts contact1("John", "+1234567890", "john@gmail.com");
+    /*Contacts contact1("John", "+1234567890", "john@gmail.com");
 
     std::cout << "Original Contact:" << std::endl;
     contact1.output();
@@ -30,7 +31,22 @@ int main() {
     contact2.Deserialize(filePath);
 
     std::cout << "\nDeserialized Contact:" << std::endl;
-    contact2.output();
+    contact2.output();*/
+
+    College college1("Base college", "Random street", 1995, 1200);
+
+    std::cout << "Original College:" << std::endl;
+    college1.output();
+
+    std::string filePath = "college_test.bin";
+    college1.Serialize(filePath);
+
+    College college2;
+
+    college2.Deserialize(filePath);
+
+    std::cout << "\nDeserialized College:" << std::endl;
+    college2.output();
 
     return 0;
 }
